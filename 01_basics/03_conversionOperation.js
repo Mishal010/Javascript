70c11ba8a4fe4355f9443717179170b0c9c3023d
Mishal010 <153232369+Mishal010@users.noreply.github.com>
2025-10-21 12:48:08 +0000

    Add operations and increment/decrement examples to conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,4 +25,73 @@ console.log(booleanIsLoggedIn)
 // 0,null,undefined,"" => false
 // "hello" => true
 
+// **************** Operations *******************
 
+let value = 3;
+let negValue = -value;
+console.log(negValue) // -3
+
+console.log(2+2) // 4
+console.log("2"+"2") // "22"
+console.log("2"+2) // "22"
+console.log(2+"2") // "22"
+
+console.log(2-"2") // 0
+console.log("4"-"2") // 2
+console.log("4abc"-"2") // NaN
+
+console.log(2*"3") // 6
+console.log("6"/"2") // 3
+console.log("6abc"/"2") // NaN
+console.log("2"*true) // 2
+console.log("2"*false) // 0
+console.log(2+true) // 3
+console.log(2+false) // 2
+console.log(2+"true") // "2true"
+console.log(2+"false") // "2false"
+console.log(true+true) // 2
+console.log(true+false) // 1
+console.log(false+false) // 0
+console.log(true-"1") // 0
+console.log(false-"1") // -1
+console.log(true*"5") // 5
+console.log(false*"5") // 0
+console.log(true/"5") // 0.2
+console.log(false/"5") // 0
+
+console.log(false/"0") // Infinity
+console.log(true/"0") // Infinity
+console.log(+"") // 0
+console.log(+null) // 0
+console.log(+undefined) // NaN
+console.log(+true) // 1
+console.log(+false) // 0
+console.log(+"33") // 33
+console.log(+"33abc") // NaN
+console.log(typeof(+"33")) // number
+console.log(typeof(+"33abc")) // number
+console.log(+"-33") // -33
+console.log(typeof(+"-33")) // number
+
+console.log(2**3) // 2^3 = 8
+console.log(9**0.5) // 3
+console.log(27**(1/3)) // 3
+console.log(16**true) // 16
+console.log(16**false) // 1
+
+// Pre and Post Increment/Decrement
+let a = 10;
+console.log(++a) // 11
+console.log(a) // 11
+
+let b = 10;
+console.log(b++) // 10
+console.log(b) // 11
+
+let c = 10;
+console.log(--c) // 9
+console.log(c) // 9
+
+let d = 10;
+console.log(d--) // 10
+console.log(d) // 9
